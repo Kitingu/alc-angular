bf849fb914b9db226fb74ccd4b47daee6be95025
benlegendj <benlegendj@gmail.com>
2019-09-27 01:18:06 +0300

    accessing static files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {EventsAppComponent } from './events-app.component';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
-  declarations: [
-    EventsAppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
+
+  declarations: [EventsAppComponent],
+
   bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2> started from the bottom now we here</h2>'
+  template: `
+  <h2> started from the bottom now we here</h2>
+  <img src="/assets/images/basic-shield.png"/>
+  `
 })
 export class EventsAppComponent {
   title = 'app';
 }
+// imports are declared in webpac config hence they are relatively available
