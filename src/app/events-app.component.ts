109fedec19f58a0f2cf57ed873c3e91b6b6f6ce1
benlegendj <benlegendj@gmail.com>
2019-09-27 00:51:23 +0300

    bootstrapping angular app

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h2> started from the bottom now we here</h2>'
+})
+export class EventsAppComponent {
+  title = 'app';
+}
