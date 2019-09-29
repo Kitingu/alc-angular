b762b5860b83944a21752f26480b1e253197befa
benlegendj <benlegendj@gmail.com>
2019-09-29 14:07:04 +0300

    communication between components using external templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventlistComponent } from "./events/events-list-component";
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 
 @NgModule({
   imports: [BrowserModule],
 
-  declarations: [EventsAppComponent,EventlistComponent],
+  declarations: [EventsAppComponent, EventlistComponent, EventThumbnailComponent],
 
   bootstrap: [EventsAppComponent]
 })
--- src/app/events/events-list-component.ts
+++ src/app/events/events-list-component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  templateUrl:'./events-list.component.html'
+  template: `<div>
+  <h1>upcoming angular events</h1>
+  <hr />
+  <event-thumbnail [event]="event1"> </event-thumbnail>
+</div>
+`
 })
 export class EventlistComponent {
-  event = {
+  event1 = {
     id: 1,
     name: 'Angular Connect',
     date: '12/12/2015',
